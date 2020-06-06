226c4eb4eec46358c4e975e327c9974e1c213e41
Saurabh Thakur <mail@saurabhthakur.dev>
2020-06-06 23:56:16 +0530

    ADD: common response structure function

--- server/handler/handler.go
+++ server/handler/handler.go
@@ -1,14 +1,34 @@
 package handler
 
-import "github.com/thakursaurabh1998/dashboarding/server/store"
+import (
+	"github.com/thakursaurabh1998/dashboarding/server/store"
+)
 
+// Handler contains all the functions required for the routes
 type Handler struct {
 	userStore store.UserStore
 }
 
+// NewHandler returns a Handler struct
 func NewHandler(us store.UserStore) (h *Handler) {
 	h = &Handler{
 		us,
 	}
 	return
+}
+
+type response struct {
+	Success   bool        `json:"success"`
+	Data      interface{} `json:"data,omitempty"`
+	Errors    []string    `json:"errors,omitempty"`
+	ErrorType string      `json:"errorType,omitempty"`
+}
+
+func createRes(success bool, data interface{}, errors []string, errorType string) *response {
+	return &response{
+		Success:   success,
+		Data:      data,
+		Errors:    errors,
+		ErrorType: errorType,
+	}
 }
